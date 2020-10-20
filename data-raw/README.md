df476116bf060bd080a6163c963e43bc0ffb14cb
George G. Vega Yon <g.vegayon@gmail.com>
2020-10-19 20:20:50 -0400

    Preparing reproducibility

--- data-raw/README.md
+++ data-raw/README.md
@@ -1,5 +1,35 @@
-The dataset `data/candidate_trees.rds` was extracted directly from
+# Data from the APHYLO paper
+
+The dataset `candidate_trees.rds` was extracted directly from
 this repository [USCbiostats/aphylo-simulations](https://github.com/USCbiostats/aphylo-simulations),
 and corresponds to the list of trees proteins that were used in the paper
 "On the automatic annotation of gene functions using phylogenetic trees".
+
+The file `mcmc_partially_annotated_no_prior.rds` corresponds to the MCMC parameter
+estimates using experimental evidence with a uniform prior for the parameters.
+
+# Data from SIFTER
+
+The folders `deaminase` and `sulfotransferase` were downloaded directly from
+the sup mats from the 2011 SIFTER paper.
+
+The `nudix` family annotations were transcribed directly from the paper, in
+particular, the tree featured in page number X.
+
+`gene_association.goa_uniprot.80.txt` which is formated in GAF 1.0
+(see http://www-legacy.geneontology.org/GO.format.gaf-1_0.shtml)
+
+Was downloaded from ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/old/UNIPROT/ version 80
+
+The file hundredfamilies was downloaded from supplemental materials of the 2011
+paper published in Genome Research. 
+
+https://genome.cshlp.org/content/21/11/1969/suppl/DC1
+
+https://ndownloader.figstatic.com/files/6649398 (see also https://doi.org/10.1371/journal.pcbi.0010045.sd001)
+From the paper website https://doi.org/10.1371/journal.pcbi.0010045 (Original 2015 paper from plos comp bio)
+
+The nudix family tree, under the `hundred` folder, was downloaded from PFAM
+version 20 (as mentioned in the paper)
+ftp://ftp.ebi.ac.uk/pub/databases/Pfam/releases/Pfam20.0/.
 
--- data/nudix.R
+++ data/nudix.R
@@ -1,20 +1,17 @@
 library(aphylo)
 library(data.table)
 
-model_aphylo <- readRDS("novel-predictions/mcmc_partially_annotated_no_prior.rds")
+model_aphylo <- readRDS("data-raw/mcmc_partially_annotated_no_prior.rds")
 model_aphylo <- window(model_aphylo, start=5000)
 
-source("sifter/read_sifter.R")
-
-# read.tree("sifter/nudix/PF00293.tree")
-
-tree <- read_nhx("sifter/PF00293.tree")
+tree <- read.tree("data-raw/nudix/PF00293.tree")
+tree$node.label <- sprintf("node%03i", seq_len(ape::Nnode(tree)))
 
 # Finding the corresponding annotations
-ann  <- fread("sifter/nudix/nudix-ann.tsv")
+ann  <- fread("data-raw/nudix/nudix-ann.tsv")
 
 # Are all the leafs in ann included in tree?
-(not_in <- which(!ann$protein %in% gsub("[/].+", "", tree$tree$tip.label)))
+(not_in <- which(!ann$protein %in% gsub("[/].+", "", tree$tip.label)))
 ann[not_in] # Protein DIPP_ASFB7 with go term 5000000 not present in nudix PFAM 20
 
 ann <- ann[-not_in,]
@@ -25,7 +22,7 @@ ann$terms <- unlist(go)
 ann[, goterms := NULL]
 length(unique(ann$terms)) # I see 68 unique terms, not 66 like mentioned in the paper.
 
-> sort(table(ann$terms))
+sort(table(ann$terms))
 # 0004170 0004382 0008727 0016887 0019177 0043135 0043262 0047693 0400000 1000000 1020000 
 #       1       1       1       1       1       1       1       1       1       1       1 
 # 1030000 1500000 1900000 2400000 2500000 3100000 3600000 4100000 4500000 4600000 4800000 
@@ -48,29 +45,31 @@ table(table(ann$terms) > 1)
 # 34    34
 
 dupl <- imputate_duplications(
-  tree$tree,
-  gsub(".+[_]|[/].+", "", tree$tree$tip.label)
+  tree,
+  gsub(".+[_]|[/].+", "", tree$tip.label)
   )
 
 ann[, value:=1L]
 anno_i <- dcast(unique(ann), protein ~ terms, value.var = "value")
 
-tree$tree$tip.label <- gsub("[/].+", "", tree$tree$tip.label)
+tree$tip.label <- gsub("[/].+", "", tree$tip.label)
 
 # Reading the aphylo tree
 ans <- aphylo_from_data_frame(
-  tree        = tree$tree,
+  tree        = tree,
   annotations = as.data.frame(anno_i),
   types       = data.frame(
-    tree$tree$node.label,
-    !dupl[-c(1:ape::Ntip(tree$tree))]
+    tree$node.label,
+    !dupl[-c(1:ape::Ntip(tree))]
   )
 )
 
 # Predictions -----------------------------------------------------------------
 
-ids <- which(rowSums(ans$tip.annotation) != Nann(ans) * 9)
-pred <- predict(model_aphylo, newdata = ans, ids = ids, loo = TRUE)
+ids  <- which(rowSums(ans$tip.annotation) != Nann(ans) * 9)
+system.time(
+  pred <- predict(model_aphylo, newdata = ans, ids = ids, loo = TRUE)
+)
 
 pred_table <- cbind(
   predicted = as.vector(pred[ids,]),
@@ -90,12 +89,138 @@ plot(pred_auc)
 
 # Computing accuracy a la SIFTER -----------------------------------------------
 
-threshold <- .5
+# Directly from the paper:
+# To compute accuracy for a given data set, we counted the number
+# of proteins for which the top-ranked prediction exactly matched
+# the experimental annotation, and divided by the total number of
+# proteins. In the case of multiple experimental annotations or top-
+#   ranked predictions, we counted the protein as having an accurate
+# prediction when the intersection of the two sets was not empty. 
 
-# Accuracy
-tpr <- sapply(ids, function(i) {
-  obs <- which(ans$tip.annotation[i,] == 1)
-  pre <- which(pred[i,] > threshold)
-  length(intersect(obs, pre) ) > 0
-}) 
+acc_at_least_one <- lapply(ids, function(i) {
+  top_pred <- which(pred[i,] == max(pred[i,]))
+  true_ann <- which(ans$tip.annotation[i,] == 1)
+  data.frame(
+    pred = paste(top_pred, collapse=","),
+    true = paste(true_ann, collapse=","),
+    accurate = ifelse(length(intersect(top_pred, true_ann)), 1, 0)
+  )
+})
+acc_at_least_one <- do.call(rbind, acc_at_least_one)
+rownames(acc_at_least_one) <- rownames(pred[ids,])
+table(acc_at_least_one$accurate)
+#  0  1 
+# 65 33 
+
+acc_at_least_one
+#               pred                true accurate
+# 16              53                  45        0
+# 31              50                  45        0
+# 32              45               45,50        1
+# 41              53            45,50,56        0
+# 42              45            25,45,54        1
+# 43           25,54                  45        0
+# 59              45                  53        0
+# 60              53                  45        0
+# 115             50                   1        0
+# 122             53                   1        0
+# 157             45       6,14,17,24,53        0
+# 182             33                   6        0
+# 193              6               33,37        0
+# 223             45               11,49        0
+# 224              1               11,49        0
+# 227             49               11,49        1
+# 231              1                  11        0
+# 322             37                   1        0
+# 331             37            36,37,44        1
+# 350             37       1,33,36,37,59        1
+# 386              5                   1        0
+# 419             36                1,36        1
+# 423             36             1,36,37        1
+# 434              1                  36        0
+# 495             37               36,37        1
+# 565             37          6,29,30,41        0
+# 621  3,10,52,61,65                  63        0
+# 668            7,9        5,6,14,17,53        0
+# 670          47,62                  66        0
+# 671             66            47,62,66        1
+# 685              5                  66        0
+# 717             24                  24        1
+# 745              5                  24        0
+# 921             42            17,24,53        0
+# 971             17                  42        0
+# 987             66             2,14,17        0
+# 991            7,9                   5        0
+# 1000             5      1,7,9,36,37,59        0
+# 1114            21               11,26        0
+# 1118            53                  11        0
+# 1134            26                  11        0
+# 1162            53                  11        0
+# 1192            26            11,21,26        1
+# 1270            53               34,40        0
+# 1281            11                  34        0
+# 1290            11                  40        0
+# 1304            40                  40        1
+# 1321            12                5,53        0
+# 1421            23                  40        0
+# 1433            11                  27        0
+# 1439            40               23,46        0
+# 1473            11          6,14,17,53        0
+# 1505            14                  11        0
+# 1530            49       6,12,17,24,53        0
+# 1556            11                  34        0
+# 1557            34            11,27,34        1
+# 1558            11                  34        0
+# 1559            34            11,27,34        1
+# 1608            34            11,13,15        0
+# 1652            68      15,49,55,58,67        0
+# 1722            68                  49        0
+# 1761            49               49,68        1
+# 1790            49            49,55,67        1
+# 1800            67                  49        0
+# 1841            49                6,53        0
+# 1866            24          6,14,17,53        0
+# 1881            24                  24        1
+# 1883            24                  24        1
+# 1886            24         24,30,35,43        1
+# 1934            53               14,17        0
+# 1940             6         19,32,39,51        0
+# 1948   19,32,39,51          6,14,17,53        0
+# 2087            63 3,10,41,46,52,61,65        0
+# 2108             8                6,53        0
+# 2114             6                   8        0
+# 2129             5                   5        1
+# 2139             5                   5        1
+# 2334             8                  57        0
+# 2371      16,60,64      22,28,57,60,64        1
+# 2443         60,64                  57        0
+# 2498      28,60,64         16,57,60,64        1
+# 2525             8                  22        0
+# 2785            11                  11        1
+# 2798            11                  11        1
+# 2878            11                  11        1
+# 2913            11                  11        1
+# 2917            11                  11        1
+# 2919            11         11,15,27,49        1
+# 2992            11               21,27        0
+# 3029            13                  11        0
+# 3058            27               11,27        1
+# 3086            11            11,31,38        1
+# 3088            11               11,31        1
+# 3105            11                  11        1
+# 3147            13            11,31,38        0
+# 3168            38   11,13,20,48,49,67        0
+# 3267            45                  11        0
+# 3300            13                4,18        0
+
+acc_all <- sapply(ids, function(i) {
+  top_pred <- which(pred[i,] == max(pred[i,]))
+  true_ann <- which(ans$tip.annotation[i,] == 1)
+  if (length(top_pred) != length(true_ann))
+    return(0)
+  ifelse(all(top_pred %in% true_ann), 1, 0)
+})
+
+mean(acc_all)
+
 
