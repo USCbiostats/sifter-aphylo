a4b2bd67604f42903eaf11558984a0871c013010
George G. Vega Yon <g.vegayon@gmail.com>
2020-10-23 03:35:53 -0400

    Now a step closer

--- data/families_data/annotations.R
+++ data/families_data/annotations.R
@@ -20,78 +20,65 @@ trees_with_more_than_2 <- functions_per_tree[n >= 2,]
 # - Are all positive
 pli_data <- dat[tree %in% trees_with_more_than_2$tree & qualifier == 1]
 
+# Getting the accession and the pfam family
+
+tmp <- lapply(seq_along(pfam_ids), function(i) {
+  data.table(
+    name = names(pfam_ids)[i],
+    fami = attr(pfam_ids[[i]]$pfam$entry$matches[[1]], "accession")
+  )
+})
+
+tmp <- rbindlist(tmp, fill = TRUE)
+tmp <- tmp[!is.na(fami)]
+
+pli_data <- merge(
+  x = pli_data,
+  y = tmp, by.x = "UniProtKB", by.y = "name",
+  all.x = TRUE, all.y = FALSE
+)
+pli_data <- pli_data[!is.na(fami)]
+
 # For the family name, I will only keep the Panther name so that way
 # I make sure that 
 
-# Getting the fasta files ------------------------------------------------------
-load("data/aphylo_families_entries.rda")
+# Families
+pfam_families <- readLines("data/families_data/annotations_families.txt")
+pfam_families <- pfam_families[!grepl("^#", pfam_families)]
+pli_data <- pli_data[fami %in% pfam_families]
 
-counter <- 0L
-fasta <- lapply(pfam_ids, function(i) {
-  
-  counter <<- counter + 1L
-  
-  d <- tryCatch(strsplit(
-    i$pfam$entry$sequence[[1]],
-    split = "(?<=.{50})",
-    perl = TRUE)[[1]],
-    error = function(e) e
-  )
-  
-  if (inherits(d, "error")) {
-    message("The protein ", counter, " has no sequence.")
-    return(NA_character_)
-  }
-  
-  paste0(
-    sprintf(">%s\n", attr(i$pfam$entry, "accession")),
-    paste(d, collapse="\n"),
-    "*")
-})
+# # Copying the families' annotations from SIFTER's data
+# all_pfam <- list.files(
+#   "SIFTER-master/large_scale_v1.0/data/families_data/annotations/",
+#   full.names = TRUE,
+#   pattern    = "*.pli.gz"
+#   )
+# 
+# # Selecting files to copy
+# all_pfam <- data.table(path = all_pfam)
+# all_pfam[, file := gsub(".+//(?=PF[0-9]+\\.)", "", path, perl = TRUE)]
+# all_pfam[, file := gsub("\\..+", "", file)]
+# all_pfam <- all_pfam[file %in% pfam_families]
+# 
+# file.copy(
+#   all_pfam$path, to = "data/families_data/annotations/",
+#   overwrite = TRUE,
+#   copy.date = TRUE
+#   )
+# 
+# PF00001 <- xml2::read_xml("data/families_data/annotations/PF00001.pli")
 
-# Removing NAs
-fasta <- fasta[!is.na(fasta)]
-
-for (f in unique(pli_data$tree)) {
-  
-  # Writing the tree
-  tmp_tree <- candidate_trees[[f]]$tree
-  tmp_tree$tip.label <-gsub(".+=", "", tmp_tree$tip.label)
-  
-  xml2::write_xml(
-    write_phyloxml(tmp_tree),
-    file = sprintf("data/aphylo_pli/reconciled_trees/%s.xml", f), 
-  )
-  # Compressing
-  # R.utils::gzip(sprintf("data/aphylo_pli/reconciled_trees/%s.xml", f), overwrite=TRUE)
+for (f in unique(pli_data$fami)) {
   
   # Creating the entire set
   write_pli(
     family_id      = f,
-    protein_name   = as.character(pli_data[tree == f, UniProtKB]),
-    protein_number = as.character(pli_data[tree == f, UniProtKB]),
-    go_number      = gsub("GO:","",as.character(pli_data[tree == f, go])),
+    protein_name   = as.character(pli_data[fami == f, UniProtKB]),
+    protein_number = as.character(pli_data[fami == f, UniProtKB]),
+    go_number      = gsub("GO:","",as.character(pli_data[fami == f, go])),
     moc            = "EXP",
-    file = sprintf("data/aphylo_pli/annotations/%s.pli", f)
+    file = sprintf("data/families_data/annotations/%s.pli", f)
   )
-  
-  # Compressing
-  # R.utils::gzip(sprintf("data/aphylo_pli/annotations/%s.pli", f), overwrite=TRUE)
-  
-  # Alignments
-  tmp_fasta <- fasta[as.character(pli_data[tree == f, UniProtKB])]
-  tmp_fasta <- unlist(tmp_fasta)
-  if (is.null(tmp_fasta)) {
-    message("No alignments for family ", f)
-    next
-  }
-  
-  cat(
-    tmp_fasta,
-    sep = "\n",
-    file = sprintf("data/aphylo_pli/alignments/%s.fasta", f)
-  )
-  R.utils::gzip(sprintf("data/aphylo_pli/alignments/%s.fasta", f), overwrite=TRUE)
   
   message("Family ", f, " done.")
   
@@ -104,23 +91,21 @@ sifter_java <- function(
   fn_ann   = sprintf("%s/annotations/%s.pli", fam_data, fam),
   fn_sifter = "/home/vegayon@PREVMED.USC.EDU/sifter-aphylo/SIFTER-master/core/sifter2.1.1.jar",
   fn_godb   = "/home/vegayon@PREVMED.USC.EDU/sifter-aphylo/SIFTER-master/large_scale_v1.0/data/goterms.sqlite"
-  
 ) {
-
+  
   tmp_d <- tempdir(check = TRUE)
-  on.exit(unlink(tmp_d, recursive = TRUE, force = TRUE))
+  # on.exit(unlink(tmp_d, recursive = TRUE, force = TRUE))
   
   fn_res <- file.path(tmp_d, "results.txt")
   dir.create(file.path(tmp_d, "results"))
   
   # Moving the files -------------------------------------------------------------
-
   if (!file.exists(fn_tree)) {
     R.utils::gunzip(
       gsub("\\.xml", ".xml.gz", fn_tree),
       destname = sprintf("%s/%s.xml", tmp_d, fam),
-      remove = FALSE
-      )
+      remove = FALSE, overwrite = TRUE
+    )
   } else {
     file.copy(fn_tree, sprintf("%s/%s.xml", tmp_d, fam))
   }
@@ -130,7 +115,7 @@ sifter_java <- function(
     R.utils::gunzip(
       gsub("\\.pli", ".pli.gz", fn_ann),
       destname = sprintf("%s/%s.pli", tmp_d, fam),
-      remove = FALSE
+      remove = FALSE, overwrite = TRUE
     )
   } else {
     file.copy(fn_ann, sprintf("%s/%s.pli", tmp_d, fam))
@@ -164,7 +149,7 @@ sifter_java <- function(
   # Trying out sifter for a single family
   cmds <- c(
     "java", sprintf(
-    '-jar -Xmx4g %s -v
+      '-jar -Xmx4g %s -v
     --with-exp --with-ida --with-ipi --with-imp --with-igi --with-iep --with-tas --with-nas
     --protein %s
     --reconciled %s
@@ -174,24 +159,31 @@ sifter_java <- function(
     --scale %s
     --alpha %s
     --truncation 1 %s', # --xvalidation --folds 0 
-    # SIFTER Path
-    fn_sifter,
-    # Protein
-    fn_ann,
-    # Tree
-    fn_tree,
-    # Ontology
-    fn_godb,
-    # Results
-    fn_res,
-    # Family file
-    tmp_infer_fx,
-    # scale
-    fn_scale,
-    fn_alpha,
-    fam
+      # SIFTER Path
+      fn_sifter,
+      # Protein
+      fn_ann,
+      # Tree
+      fn_tree,
+      # Ontology
+      fn_godb,
+      # Results
+      fn_res,
+      # Family file
+      tmp_infer_fx,
+      # scale
+      fn_scale,
+      fn_alpha,
+      fam
     ))
   cmds[2] <- gsub("\n", " ", cmds[2])
   system2(cmds[1], cmds[2])
-
+  
 }
+
+ans <- sifter_java(
+  fam = "PF12474",
+  fam_data = "data/families_data/",
+  fn_tree = "SIFTER-master/large_scale_v1.0/data/families_data/reconciled_trees/PF12474_reconciled.xml",
+  fn_ann = "data/families_data/annotations/PF12474.pli",
+)
--- sifter-runs/run.R
+++ sifter-runs/run.R
@@ -1,17 +1,22 @@
 # In order for this script to work, the file sifter_db_pass.txt must exists
 # at the top level of this project.
 
+family_map <- NULL
+
 #' This is a wrapper that runs SIFTER
 #' @param fam A family in the form "PTHR31682_taxa-3702"
 #' @param dbpass Password for the database
 #' @param path Path to where all the families (fasta, pfamscan, protein list)
 #' @param sifter Path to SIFTER's scripts.
+#' @param timeout 
 #' @return A list with timings and other relevant data.
 run_sifter_on_aphylo <- function(
   fam,
   dbpass,
-  path   = "data/aphylo_families/",
-  sifter = "SIFTER-master/large_scale_v1.0/scripts/"
+  path    = "data/aphylo_families/",
+  sifter  = "SIFTER-master/large_scale_v1.0/scripts/",
+  fam_data = "SIFTER-master/large_scale_v1.0/data/families_data/",
+  timeout = 1
   ) {
   
   message(paste(rep("-", getOption("width", 80)), collapse = ""))
@@ -42,21 +47,18 @@ run_sifter_on_aphylo <- function(
     print(out_step2)
     return(NULL)
   } else {
-    message(
-      "Done! Families:",
-      paste(
-        readLines(sprintf("%s/family_list.txt", tmp_dir)),
-        collapse=", "
-        )
-      )
+    famlist <- readLines(sprintf("%s/family_list.txt", tmp_dir))
+    message("Done! Families:", paste(famlist, collapse=", "))
+    
+    family_map <<- c(family_map, structure(paste(famlist, collapse=", "), names = fam))
   }
   
   # Step 3: --------------------------------------------------------------------
   message("sifter_prepare ... ", appendLF = FALSE)
   time_step3 <- proc.time()
   cmd <- sprintf(
-    "%s/sifter_prepare.py --dbpass=%s --ip %s/%s_proteins.txt %s/../data/families_data/ %s",
-    sifter, dbpass, tmp_dir, fam, sifter, tmp_dir
+    "%s/sifter_prepare.py --dbpass=%s --ip %s/%s_proteins.txt %s %s",
+    sifter, dbpass, tmp_dir, fam, fam_data, tmp_dir
   )
   
   out_step3 <- tryCatch(
@@ -70,6 +72,33 @@ run_sifter_on_aphylo <- function(
   } else {
     message("Done!")
   }
+  
+  # Checking the timing
+  which_time <- which(grepl("^Total estimated time", out_step3))
+  if (!length(which_time)) {
+    message("Couldn't measure estimated time. Trying with the next.")
+    cat(out_step3, sep = "\n")
+    return(NULL)
+  }
+  
+  # checking time
+  time_estimate <- gsub(".+your query is\\s+|\\s+\\(.+", "", out_step3[which_time[1]])
+  time_unit <- gsub(".+\\s+", "", time_estimate)
+  time_estimate <- as.numeric(gsub("\\s+.+", "", time_estimate))
+  if (time_unit == "hours") {
+    time_estimate <- time_estimate*60
+  } else if (time_unit == "minutes") {
+    time_estimate <- time_estimate
+  } else if (time_unit == "days")
+    time_estimate <- time_estimate*60*24
+  
+  if (time_estimate >= timeout) {
+    message("The estimate was ", time_estimate, "minutes which is above the time out.")
+    message(out_step3[which_time[1]], sep = "\n")
+    return(NULL)
+  } else
+    message(out_step3[which_time[1]])
+  
   
   # Step 4 ---------------------------------------------------------------------
   message("sifter_run_cv ... ", appendLF = FALSE)
@@ -122,18 +151,37 @@ run_sifter_on_aphylo <- function(
   
   
 }
-
+# debug(run_sifter_on_aphylo)
 SIFTER_PASS <- readLines("sifter_db_pass.txt")
 
 # Listing all the families
 families <- list.files(path = "data/aphylo_families/", pattern = "*.fasta")
 families <- setdiff(unique(gsub("[.].+", "", families)), "all_families")
 
-for (f in families) {
+# One example The file PTHR10024_taxa-10090 is mapped to PF00168
+
+PF00168 <- xml2::read_xml("data/families_data/annotations/PF00168.pli")
+ids_out <- length(xml2::xml_find_all(PF00168, "//GONumber"))
+set.seed(12312)
+ids_out <- sample.int(ids_out, ids_out - 10)
+xml2::xml_remove(xml2::xml_find_all(PF00168, "//GONumber")[ids_out])
+xml2::write_xml(PF00168, file = "data/families_data/annotations/PF00168.pli")
+R.utils::gzip("data/families_data/annotations/PF00168.pli", overwrite = TRUE)
+
+ans <- run_sifter_on_aphylo("PTHR10024_taxa-10090", SIFTER_PASS, fam_data = "data/families_data")
+
+failed <- NULL
+for (f in families[1:10]) {
   
-  ans <- run_sifter_on_aphylo(f, SIFTER_PASS)
+  ans <- run_sifter_on_aphylo(f, SIFTER_PASS, fam_data = "data/families_data")
+  
+  if (!length(ans)) {
+    failed <- c(failed, f)
+    next
+  }
+  
   saveRDS(ans, file = sprintf("sifter-runs/%s.rds", f))
   
 }
 
-  
+# saveRDS(family_map, "sifter-runs/run.rds")
